410167b049e1946139adc914b07a39c403afe7dc
srgmatveev <srgmatveev@gmail.com>
2020-07-19 14:20:59 +0300

    19 July 2020

--- hw01_csv/src/main/java/dao/CSVQuestionDao.java
+++ hw01_csv/src/main/java/dao/CSVQuestionDao.java
@@ -3,6 +3,7 @@ package dao;
 import org.sergio.domain.Question;
 
 import java.nio.file.Path;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -18,8 +19,22 @@ public class CSVQuestionDao implements QuestionDao {
 
     private Question parseString(String str) {
         if (str == null || str.isEmpty()) return new Question();
-
-        return new Question();
+        List<String> list = parsePattern(str);
+        if (list.size() < 3) return new Question();
+        String questionStr = list.get(0);
+        int answer = 0;
+        try {
+            answer = Integer.parseInt(list.get(list.size() - 1));
+            if (answer == 0 || answer > list.size() - 2)
+                throw new NumberFormatException("wrong answer");
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return new Question();
+        }
+        list.remove(0);
+        list.remove(list.size() - 1);
+        Question question = new Question(questionStr, list, answer);
+        return question;
     }
 
     private List<String> parsePattern(String text) {
@@ -28,7 +43,7 @@ public class CSVQuestionDao implements QuestionDao {
         Pattern pattern = Pattern.compile("(?:^|,)\\s*(?:(?:(?=\")\"([^\"].*?)\")|(?:(?!\")(.*?)))(?=,|$)");
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
-            String tmp = matcher.group(1)==null? matcher.group(2):matcher.group(1);
+            String tmp = matcher.group(1) == null ? matcher.group(2) : matcher.group(1);
             list.add(tmp);
         }
         return list;
