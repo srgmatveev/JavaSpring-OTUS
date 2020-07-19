061d06cc7491acacdd61ff7d4e8a3c5f3484b8e9
srgmatveev <srgmatveev@gmail.com>
2020-07-19 21:48:33 +0300

    19 July 2020

--- hw01_csv/src/main/java/org/sergio/Main.java
+++ hw01_csv/src/main/java/org/sergio/Main.java
@@ -4,12 +4,20 @@ import org.sergio.domain.Person;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import service.ConsoleHelper;
+
+import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring.xml");
-        Person person = (Person) context.getBean("person","Sergey","Matveev");
+
+        ConsoleHelper.writeMessage("Введите имя");
+        String name = ConsoleHelper.readMessage();
+        ConsoleHelper.writeMessage("Введите фамилию");
+        String surName = ConsoleHelper.readMessage();
+        Person person = (Person) context.getBean("person",name,surName);
         System.out.println(person.getName());
         System.out.println(person.getSurName());
     }
--- hw01_csv/src/main/java/service/ConsoleHelper.java
+++ hw01_csv/src/main/java/service/ConsoleHelper.java
@@ -1,6 +1,23 @@
 package service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader;
+
+    static {
+        InputStream is = System.in;
+        InputStreamReader isr = new InputStreamReader(is);
+        reader = new BufferedReader(isr);
+    }
+
+    public static String readMessage() throws IOException {
+        return reader.readLine().trim();
+    }
+
     public static void writeMessage(String message) {
         System.out.println(message);
     }
