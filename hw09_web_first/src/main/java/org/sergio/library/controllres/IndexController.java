2a6c88b2c5ebd02916397756142e24beae42dc70
srgmatveev <srgmatveev@gmail.com>
2020-10-01 21:27:38 +0300

    01 October

--- hw09_web_first/src/main/java/org/sergio/library/controllres/IndexController.java
+++ hw09_web_first/src/main/java/org/sergio/library/controllres/IndexController.java
@@ -1,12 +1,24 @@
 package org.sergio.library.controllres;
 
+import org.sergio.library.domain.Genre;
+import org.sergio.library.repository.GenreRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
+    @Autowired
+    GenreRepo genreRepo;
+
     @GetMapping("/")
-    String index(){
+    String index(Model model){
+        List<Genre> genreList = genreRepo.findAll(Sort.by(Sort.Direction.ASC, "name"));
+        model.addAttribute("genres", genreList);
         return "index";
     }
 }
