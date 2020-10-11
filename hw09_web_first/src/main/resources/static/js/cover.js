01fbba089ea5fb85a1b6c581dc9524541e3c4fb6
srgmatveev <srgmatveev@gmail.com>
2020-10-11 19:40:48 +0300

    cover.js await modify

--- hw09_web_first/src/main/resources/static/js/cover.js
+++ hw09_web_first/src/main/resources/static/js/cover.js
@@ -10,17 +10,20 @@ function delete_image(elem, quest, ready_text, error_text, catch_text) {
 
 async function del_fetch(id, ready_text, error_text, catch_text) {
     let url = `/covers/del/${id}`;
-    fetch(url, {
-        method: 'DELETE',
-    })
-        .then((response) => {
-            if (response.ok) {
-                console.log(ready_text);
-            }
-            else
-                throw new Error(error_tex)
-        })
-        .catch(() => { console.log(catch_text); })
+
+    try {
+        let responce = await fetch(url, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            console.log(ready_text);
+            return;
+        }
+        throw new Error(error_tex);
+    } catch (err) {
+        console.log(`${catch_text} : ${error_text}`);
+    }
 }
 
 window.onload = function () {
