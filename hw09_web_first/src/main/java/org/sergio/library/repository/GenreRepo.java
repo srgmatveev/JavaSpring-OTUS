3f7ac4bf3258a9f997b59e8dc1e65cda19aaf7dc
srgmatveev <srgmatveev@gmail.com>
2020-10-14 13:14:08 +0300

    remove cachable

--- hw09_web_first/src/main/java/org/sergio/library/repository/GenreRepo.java
+++ hw09_web_first/src/main/java/org/sergio/library/repository/GenreRepo.java
@@ -1,6 +1,7 @@
 package org.sergio.library.repository;
 
 import org.sergio.library.domain.Genre;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -12,6 +13,15 @@ public interface GenreRepo extends MongoRepository<Genre, String> {
 
     @Query("{ 'name' : {$regex: '^?0$', $options: 'i' }}")
     Genre findByName(final String Name);
+
+    @Override
+    <S extends Genre> S save(S entity);
+
+    @Override
+    <S extends Genre> S insert(S entity);
+
+    @Override
+    void deleteById(String s);
 
     default Genre uniqSave(Genre genre) {
         if(genre==null) return null;
