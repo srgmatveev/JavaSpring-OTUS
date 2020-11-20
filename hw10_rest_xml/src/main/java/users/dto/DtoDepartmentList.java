9e77960d8faee1529b576dc1e9655057e7b646f8
sergio <srgmatveev@gmail.com>
2020-11-20 12:23:26 +0300

    fix name Department List item to department

--- hw10_rest_xml/src/main/java/users/dto/DtoDepartmentList.java
+++ hw10_rest_xml/src/main/java/users/dto/DtoDepartmentList.java
@@ -3,15 +3,28 @@ package users.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-@XmlRootElement(name = "departments")
-@Data
+@XmlRootElement(name="departments")
 @NoArgsConstructor
+@Data
 public class DtoDepartmentList implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @XmlElement(name="department")
+    public List<DtoDepartment> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(List<DtoDepartment> departments) {
+        this.departments = departments;
+    }
+
+
     private List<DtoDepartment> departments = new ArrayList<>();
 }
