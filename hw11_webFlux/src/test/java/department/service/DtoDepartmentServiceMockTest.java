7722ab3e374f5a9771434300e193b17513c5fc9d
sergio <srgmatveev@gmail.com>
2020-11-23 21:06:06 +0300

    Test for DToDepartment webFlux add Mock

--- hw11_webFlux/src/test/java/department/service/DtoDepartmentServiceMockTest.java
+++ hw11_webFlux/src/test/java/department/service/DtoDepartmentServiceMockTest.java
@@ -1,64 +1,48 @@
 package department.service;
 
-import department.Main;
+import department.dao.DepartmentRepo;
+import department.domain.Department;
 import department.dto.DtoDepartment;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-
-
-@SpringBootTest(classes = Main.class) //(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class DtoDepartmentServiceTest {
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class DtoDepartmentServiceMockTest {
     @Autowired
     @Qualifier("DtoDepartmentService")
     private DepartmentService service;
 
 
+    @MockBean
+    private DepartmentRepo repo;
+
     @Test
     @SuppressWarnings("unchecked")
-    void createDepartment() {
-        final String name = "Sergio1";
+    void createDepartment_switchIfEmpty() {
+        Department department = new Department();
+        department.setName("Sergio");
+
+        Mockito.when(repo.insert(department)).thenReturn(Mono.empty());
+
         DtoDepartment dtoDepartment = new DtoDepartment();
-        dtoDepartment.setName(name);
+        dtoDepartment.setName("Sergio");
         Mono<DtoDepartment> resultMono = service.createDepartment(dtoDepartment);
         StepVerifier
                 .create(resultMono)
-                .assertNext(dep->{
-                    assertNotNull(dep);
-                    assertNotNull(dep.getId());
-                    assertEquals(dep.getName(), name);
-                })
-                .expectComplete()
+                .expectErrorMessage("No Department created with Name: Sergio")
                 .verify();
-
     }
 
-   /* @Test
-    void updateDepartment() {
-    }
-
-    @Test
-    void findAll() {
-    }
-
-    @Test
-    void findOne() {
-    }
-
-    @Test
-    void findByName() {
-    }
-
-    @Test
-    void delete() {
-    }
-
-    */
 }
