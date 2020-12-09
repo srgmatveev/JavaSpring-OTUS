a2484eb536de07418247e4dbfc71ba1c7d810745
sergio <srgmatveev@gmail.com>
2020-12-09 21:20:56 +0300

    09 Dec books form

--- hw14_acl/src/main/java/org/sergio/library/controllres/BookController.java
+++ hw14_acl/src/main/java/org/sergio/library/controllres/BookController.java
@@ -1,7 +1,50 @@
 package org.sergio.library.controllres;
 
+import lombok.extern.slf4j.Slf4j;
+import org.sergio.library.domain.Author;
+import org.sergio.library.dto.BookDTO;
+import org.sergio.library.repository.AuthorRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@Slf4j
+@RequestMapping(value = "/books")
 public class BookController {
+    @Autowired
+    private AuthorRepo authorRepo;
+
+    @GetMapping
+    String getBooks(Model model) {
+        return "book/books";
+    }
+
+    @GetMapping(value = "add")
+    String addBook(Model model) {
+        BookDTO bookDTO = new BookDTO();
+        bookDTO.setId("1111");
+        model.addAttribute("book", bookDTO);
+        return "book/add_book";
+    }
+
+    @GetMapping(value = "add_authors")
+    String add_author(Model model) {
+        List<Sort.Order> orders = new ArrayList<>();
+        Sort.Order orderName = new Sort.Order(Sort.Direction.ASC, "name");
+        Sort.Order orderSurName = new Sort.Order(Sort.Direction.ASC, "surName");
+        orders.add(orderSurName);
+        orders.add(orderName);
+        List<Author> list = authorRepo.findAll(Sort.by(orders));
+        model.addAttribute("authors", list);
+
+        return "book/add_author";
+    }
+
 }
---
+++ hw14_acl/src/main/resources/static/js/add-author-to-book.js
@@ -0,0 +1,12 @@
+function add_book_authors(url_path, alert_message) {
+    const id = document.getElementById("id");
+    if (id == undefined) {
+        console.error("Not defined element with id == 'id'");
+        return;
+    }
+    if (!id.value) {
+          alert(`${alert_message}`);
+        return;
+    }
+    window.open(url_path,"_self");
+}
